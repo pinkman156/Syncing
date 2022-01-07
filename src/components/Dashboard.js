cdb5f1dc2d6c74cdc12f56744eba071c8df85e25
Nipun Kohli <nipunk75@gmail.com>
2022-01-08 01:47:27 +0530

    display name on login

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,35 @@
 import { React, useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+
+var cors = require("cors");
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "24d8f710781e47c1b3d37a5073abb12d",
+});
 
 export default function Dashboard(props) {
   const accessToken = props.token;
 
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    const res = spotifyApi.getMe().then((e) => {
+      setName(e.body.display_name);
+    });
+    // if (accessToken) {
+    //   getTrack();
+    //   setLoad(true);
+    // }
+  }, [accessToken]);
+
   return (
     <div>
-      <h1>logged in</h1>
+      <h1>logged in {$name}</h1>
     </div>
   );
 }
