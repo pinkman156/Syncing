d6f196bc515b96f2c290da7c4938e8c31d9a91a9
Nipun Kohli <nipunk75@gmail.com>
2022-01-08 01:22:09 +0530

    changed redirecr uri

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "24d8f710781e47c1b3d37a5073abb12d";
-export const redirectUri = "https://moodify-sable.vercel.app/redirect";
+export const redirectUri = "https://syncing.vercel.app/redirect";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
